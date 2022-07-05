386575bb49c2da0c98f4d48878b1f0d7b438c25c
Jenipher Belloso <jenipherbelloso@gmail.com>
2022-07-05 14:50:43 -0600

    commit tree 74905cce464cfc3c68aa541d3f65ada1b0c8e400

--- index.js
+++ index.js
@@ -12,5 +12,38 @@ const tutorials = [
 ];
 
 const titleCased = () => {
+
+UpperCaseTest(tutorials);  
   return tutorials
 }
+
+function UpperCaseTest(tutorials)
+  {
+    for(let i = 0; i < tutorials.length; i++)
+    {
+      let sentance = tutorials[i];
+      let words = sentance.split(' ');
+      
+      for(let j = 0; j < words.length; j++)
+      {
+        words[j] = words[j][0].toUpperCase() + words[j].substr(1);
+      }
+
+      words.join(' ');
+
+      sentance = words;
+      tutorials[i] = sentance;
+    }
+
+  }
+
+
+//function titleCased(){
+  //return titleCased.charAt(0).toUpperCase() + titleCased.slice(1);
+  //return titleCased.map(tutorials => {
+    //return tutorials.charAt(0).toUpperCase() + tutorials.sustring(1).toLowerCase(); 
+  //})
+//}
+
+//const caps = tutorials.split(' ').map(capitalize).join(' ')
+//console.log(titleCased(['what does the this keyword mean?']));
